c22feead5f899b655270c8655d6a800611077d9b
Shii Kayano <bryghtshadow@live.com>
2011-09-19 16:43:32 +1200

    Adding ImageSelectionDialog used for selecting images + add ImageSelectionDialog view

---
+++ BaconInfoGen/BaconBuilder/View/ImageSelectionDialog.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace BaconBuilder.View
+{
+	partial class ImageSelectionDialog
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.lblImageLocation = new System.Windows.Forms.Label();
+			this.txtImageURL = new System.Windows.Forms.TextBox();
+			this.btnBrowse = new System.Windows.Forms.Button();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.btnCancel = new System.Windows.Forms.Button();
+			this.btnOK = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.flowLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.tableLayoutPanel1);
+			this.panel1.Controls.Add(this.flowLayoutPanel1);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.panel1.Location = new System.Drawing.Point(0, 0);
+			this.panel1.Name = "panel1";
+			this.panel1.Padding = new System.Windows.Forms.Padding(20);
+			this.panel1.Size = new System.Drawing.Size(594, 150);
+			this.panel1.TabIndex = 3;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+			this.tableLayoutPanel1.Controls.Add(this.lblImageLocation, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.txtImageURL, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.btnBrowse, 1, 1);
+			this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(20, 20);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle());
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(554, 81);
+			this.tableLayoutPanel1.TabIndex = 6;
+			// 
+			// lblImageLocation
+			// 
+			this.lblImageLocation.AutoSize = true;
+			this.lblImageLocation.Location = new System.Drawing.Point(3, 0);
+			this.lblImageLocation.Name = "lblImageLocation";
+			this.lblImageLocation.Size = new System.Drawing.Size(83, 13);
+			this.lblImageLocation.TabIndex = 0;
+			this.lblImageLocation.Text = "Image Location:";
+			// 
+			// txtImageURL
+			// 
+			this.txtImageURL.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtImageURL.Location = new System.Drawing.Point(3, 16);
+			this.txtImageURL.Name = "txtImageURL";
+			this.txtImageURL.Size = new System.Drawing.Size(467, 20);
+			this.txtImageURL.TabIndex = 1;
+			// 
+			// btnBrowse
+			// 
+			this.btnBrowse.Location = new System.Drawing.Point(476, 16);
+			this.btnBrowse.Name = "btnBrowse";
+			this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+			this.btnBrowse.TabIndex = 2;
+			this.btnBrowse.Text = "Browse...";
+			this.btnBrowse.UseVisualStyleBackColor = true;
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.AutoSize = true;
+			this.flowLayoutPanel1.Controls.Add(this.btnCancel);
+			this.flowLayoutPanel1.Controls.Add(this.btnOK);
+			this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(20, 101);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(554, 29);
+			this.flowLayoutPanel1.TabIndex = 5;
+			// 
+			// btnCancel
+			// 
+			this.btnCancel.Location = new System.Drawing.Point(476, 3);
+			this.btnCancel.Name = "btnCancel";
+			this.btnCancel.Size = new System.Drawing.Size(75, 23);
+			this.btnCancel.TabIndex = 6;
+			this.btnCancel.Text = "Cancel";
+			this.btnCancel.UseVisualStyleBackColor = true;
+			// 
+			// btnOK
+			// 
+			this.btnOK.Location = new System.Drawing.Point(395, 3);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.TabIndex = 5;
+			this.btnOK.Text = "OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			// 
+			// ImageSelectionDialog
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(594, 150);
+			this.Controls.Add(this.panel1);
+			this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+			this.Name = "ImageSelectionDialog";
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+			this.Text = "Select Image";
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.flowLayoutPanel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.Label lblImageLocation;
+		private System.Windows.Forms.TextBox txtImageURL;
+		private System.Windows.Forms.Button btnBrowse;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Button btnCancel;
+		private System.Windows.Forms.Button btnOK;
+
+	}
+}
---
+++ BaconInfoGen/BaconBuilder/View/ImageSelectionDialog.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace BaconBuilder.View
+{
+	public partial class ImageSelectionDialog : Form
+	{
+		OpenFileDialog openFileDialog;
+		const string bmp = "*.bmp";
+		const string gif = "*.gif";
+		const string[] jpeg = new [] {"*.jpg","*.jpe","*.jpg;"};
+
+		public ImageSelectionDialog()
+		{
+			InitializeComponent();
+
+			openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "All Image Files|*.bmp; *.gif; *.jpg; *.jpe; *.jpg; *.png|Bitmap Files|*.bmp|";
+
+			this.CancelButton = btnCancel;
+			this.AcceptButton = btnOK;
+
+			this.btnBrowse.Click += new System.EventHandler(this.btnBrowser_Click);
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+		}
+
+		private void btnBrowser_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+			this.Tag = txtImageURL.Text;
+		}
+
+		private void btnCancel_Click(object sender, EventArgs e)
+		{
+			
+		}
+	}
+}
