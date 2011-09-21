def5c02f332fa0619064ac26692293bf6c95c963
Shii Kayano <bryghtshadow@live.com>
2011-09-21 16:52:14 +1200

    Cleaning up un-needed QR encoder source code (30kb)

---
+++ BaconInfoGen/BaconBuilder/Model/QrCodeGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ThoughtWorks.QRCode.Codec;
+using System.Drawing;
+
+namespace BaconBuilder.Model
+{
+    public class QrCodeGenerator
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="data">the string of data to convert to a qr code</param>
+        /// <returns>the image of the qr code generated</returns>
+        public Image GenerateCode(string data)
+        {
+            QRCodeEncoder qrCodeEncoder = new QRCodeEncoder();
+
+            //sets encode mode to encode as bytes
+            qrCodeEncoder.QRCodeEncodeMode = QRCodeEncoder.ENCODE_MODE.BYTE;
+            qrCodeEncoder.QRCodeVersion = 4;
+            qrCodeEncoder.QRCodeScale = 5;
+            Image qrCode = qrCodeEncoder.Encode(data);
+            return qrCode;
+        }
+    }
+}
