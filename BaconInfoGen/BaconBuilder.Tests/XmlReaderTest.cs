9bbbb6471b5b61ea03fbf51244b53f3aba8243bc
Shii Kayano <bryghtshadow@live.com>
2011-10-03 17:52:25 +1300

    Almost perfect reading in of HTML

--- BaconInfoGen/BaconBuilder.Tests/XmlReaderTest.cs
+++ BaconInfoGen/BaconBuilder.Tests/XmlReaderTest.cs
@@ -1,4 +1,8 @@
-﻿using BaconBuilder.Properties;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using BaconBuilder.Model;
+using BaconBuilder.Properties;
 using NUnit.Framework;
 
 namespace BaconBuilder
@@ -8,25 +12,61 @@ namespace BaconBuilder
 	{
 		#region Setup/Teardown
 
+		[TestFixtureSetUp]
+		public void TestFixtureSetUp()
+		{
+			Directory.CreateDirectory(Dir);
+			
+			File.WriteAllText(Dir + FileName,
+Contents);
+		}
+
 		[SetUp]
 		public void Setup()
 		{
-			_reader = new Reader();
+			_reader = new Reader {Page = new FileInfo(Dir + FileName)};
+		}
+
+		[TearDown]
+		public void Teardown()
+		{
+			_reader = null;
+		}
+
+		[TestFixtureTearDown]
+		public void TestFixtureTearDown()
+		{
+			File.Delete(Dir + FileName);
+			Directory.Delete(Dir);
 		}
 
 		#endregion
 
 		private Reader _reader;
+		private static readonly string Dir = "C:/Users/" + Environment.UserName + "/test/";
+		private const string FileName = "ParseTest.html";
+		private const string Contents = @"<!DOCTYPE HTML>
+<html>
+	<head>
+		<link href=""style.css"" rel=""stylesheet"" />
+		<title></title>
+		<!-- Push Button = Receive Bacon --><!-- x = 42 --><!-- y = 13 -->
+	</head>
+	<body>
+		<p>hello world</p>
+		<p>ASDF</p>
+		<div>
+			<img src=""sample.jpg"" />
+			<audio src=""sample.mp3"" controls=""controls""></audio>
+		</div>
+		<p>hello you</p>
+	</body>
+</html>";
 
 		[Test]
-		public void EnsureTestHtmlIsCorrect()
+		public void TestBody()
 		{
-			const string expected = 
-				@"<!DOCTYPE HTML><html><head><link href=""style.css"" rel=""stylesheet"" />" +
-				@"<title></title><!-- x = 42 --><!-- y = 13 --></head><body><p>hello world</p><img src=""sample.jpg"" /><p>hello you</p></body></html>";
-
-			Assert.AreNotEqual("", Resources.TestFile);
-			//Assert.AreEqual(expected, Resources.TestFile);
+			Assert.AreEqual("<p>hello world</p><p>ASDF</p><div><img src=\"sample.jpg\" /><audio src=\"sample.mp3\" controls=\"controls\"></audio></div><p>hello you</p>", _reader.GetBody());
 		}
 
 		[Test]
@@ -36,20 +76,16 @@ namespace BaconBuilder
 			Assert.AreEqual("y = 13", _reader.GetProperties("y"));
 			Assert.AreEqual("x = 42", _reader.GetProperties("x"));
 			Assert.AreEqual("Push Button = Receive Bacon", _reader.GetProperties("push button"));
-		}
-
-		[Test]
-		public void TestBody()
-		{
-			Assert.AreEqual("<p>hello world</p><img src=\"sample.jpg\" /><p>hello you</p>", _reader.GetBody());
+			Assert.IsNull(_reader.GetProperties("property does not exist"));
 		}
 
 		[Test]
 		public void TestPara()
 		{
 			Assert.AreEqual("hello world", _reader.GetBodyContents()[0], "Tags were not split");
-			Assert.AreEqual("<img>sample.jpg</img>", _reader.GetBodyContents()[1]);
-			Assert.AreEqual("hello you", _reader.GetBodyContents()[2]);
+			Assert.AreEqual("ASDF", _reader.GetBodyContents()[1]);
+			//Assert.AreEqual("<img>sample.jpg</img>", _reader.GetBodyContents()[1]);
+			//Assert.AreEqual("hello you", _reader.GetBodyContents()[2]);
 		}
 	}
 }
